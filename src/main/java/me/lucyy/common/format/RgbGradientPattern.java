08fcef88245463ce91f2a4967a7638635f2b3553
Lucy Poulton <lucy@poulton.xyz>
2021-03-10 16:59:12 +0000

    added command helper, fix rgb gradient bug

--- src/main/java/me/lucyy/common/format/RgbGradientPattern.java
+++ src/main/java/me/lucyy/common/format/RgbGradientPattern.java
@@ -11,18 +11,23 @@ public class RgbGradientPattern implements FormatPattern {
 
 	public static String fade(String col1, String col2, String formats, String text) {
 		Color color1 = Color.decode(col1);
-		Color color2 = Color.decode(col2);
 
+		StringBuilder formatters = new StringBuilder();
+		if (formats != null) {
+			for (char character : formats.toCharArray()) formatters.append("&").append(character);
+		}
+
+		// special case for single-length characters
+		if (text.length() == 1) return ChatColor.of(color1) + formatters.toString() + text;
+
+		Color color2 = Color.decode(col2);
 		StringBuilder output = new StringBuilder();
 
 		int[] reds = TextFormatter.fade(text.length(), color1.getRed(), color2.getRed());
 		int[] greens = TextFormatter.fade(text.length(), color1.getGreen(), color2.getGreen());
 		int[] blues = TextFormatter.fade(text.length(), color1.getBlue(), color2.getBlue());
 
-		StringBuilder formatters = new StringBuilder();
-		if (formats != null) {
-			for (char character : formats.toCharArray()) formatters.append("&").append(character);
-		}
+
 		for (int x = 0; x < text.length(); x++) {
 			output.append(ChatColor.of(new Color(reds[x], greens[x], blues[x])));
 			output.append(formatters.toString());
