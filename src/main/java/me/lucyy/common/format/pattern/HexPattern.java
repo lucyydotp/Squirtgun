b56e1e9d3a01d016d5b2d3f005ac4f1f754edb0b
Lucy Poulton <lucy@poulton.xyz>
2021-04-03 13:54:05 +0100

    removed old hex pattern method

--- src/main/java/me/lucyy/common/format/pattern/HexPattern.java
+++ src/main/java/me/lucyy/common/format/pattern/HexPattern.java
@@ -5,11 +5,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class HexPattern implements FormatPattern {
-    final Pattern pattern = Pattern.compile("\\{(#[A-Fa-f0-9]{6})}");
 
     @Override
     public TextComponent process(String in, String formatters) {
@@ -22,20 +18,5 @@ public class HexPattern implements FormatPattern {
         TextColor colour = TextFormatter.colourFromText(in.substring(1, 8));
         assert colour != null; // this is just to shut intellij up
         return Component.text(in.substring(9), TextColor.color(colour));
-    }
-
-    public String processOld(String in, String format) {
-        Matcher matcher = pattern.matcher(in);
-        while (matcher.find()) {
-            String color = matcher.group(1);
-
-            StringBuilder formatters = new StringBuilder();
-            if (format != null) {
-                for (char character : format.toLowerCase().toCharArray()) formatters.append("&").append(character);
-            }
-
-            in = in.replace(matcher.group(), TextFormatter.colourFromTextOld(color) + formatters.toString());
-        }
-        return in;
     }
 }
