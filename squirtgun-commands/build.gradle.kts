be237234424054db196e3f24625e0c6192d407d7
Lucy Poulton <lucy@poulton.xyz>
2021-05-21 17:19:17 +0100

    hmm yes mit

--- squirtgun-commands/build.gradle.kts
+++ squirtgun-commands/build.gradle.kts
@@ -1,3 +1,26 @@
+/*
+ * Copyright © 2021 Lucy Poulton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 description = "squirtgun-commands"
 
 plugins {
@@ -18,9 +41,9 @@ dependencies {
     testImplementation("com.google.guava:guava:30.1.1-jre")
     testImplementation(project(":squirtgun-api"))
 
-    compileOnly("net.kyori:adventure-api:4.7.0")
-    compileOnly("com.google.guava:guava:30.1.1-jre")
-    compileOnly(project(":squirtgun-api"))
+    implementation("net.kyori:adventure-api:4.7.0")
+    implementation("com.google.guava:guava:30.1.1-jre")
+    implementation(project(":squirtgun-api"))
 }
 
 tasks {
