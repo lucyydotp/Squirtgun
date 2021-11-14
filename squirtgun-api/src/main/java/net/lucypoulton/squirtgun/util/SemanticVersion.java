8bf30a10982e1521abbefd16f01cd171ce1e1ec6
Lucy Poulton <lucy@lucypoulton.net>
2021-11-14 16:11:22 +0000

    initial implementation of a semantic version class with basic parsing and ordering tests

---
+++ squirtgun-api/src/main/java/net/lucypoulton/squirtgun/util/SemanticVersion.java
@@ -0,0 +1,108 @@
+package net.lucypoulton.squirtgun.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A semantic version.
+ *
+ * @see <a href="https://semver.org">https://semver.org</a>
+ */
+public class SemanticVersion implements Comparable<SemanticVersion> {
+
+    public static Pattern parsePattern =
+            Pattern.compile("^(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)(-(?<prerelease>[0-9A-Za-z-.]+))?(\\+(?<metadata>[0-9A-Za-z-.]+))?$");
+
+    public static SemanticVersion parse(CharSequence input) {
+        Matcher matcher = parsePattern.matcher(input);
+        if (!matcher.find()) throw new IllegalArgumentException("Not a valid semantic version");
+        String prerelease = matcher.group("prerelease");
+
+        return new SemanticVersion(
+                Integer.parseInt(matcher.group("major")),
+                Integer.parseInt(matcher.group("minor")),
+                Integer.parseInt(matcher.group("patch")),
+                matcher.group("metadata"),
+                prerelease == null ? new String[]{} : prerelease.split("\\.")
+        );
+    }
+
+    private final int major;
+    private final int minor;
+    private final int patch;
+    private final String[] prerelease;
+    private final @Nullable String build;
+
+    public SemanticVersion(int major, int minor, int patch, String... prerelease) {
+        this(major, minor, patch, null, prerelease);
+    }
+
+    public SemanticVersion(int major, int minor, int patch, @Nullable String build, String... prerelease) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.prerelease = prerelease;
+        this.build = build;
+    }
+
+    public int major() {
+        return major;
+    }
+
+    public int minor() {
+        return minor;
+    }
+
+    public int patch() {
+        return patch;
+    }
+
+    public String[] prerelease() {
+        return prerelease;
+    }
+
+    public @Nullable String build() {
+        return build;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SemanticVersion) {
+            SemanticVersion ver = (SemanticVersion) obj;
+            return this.major() == ver.major()
+                    && this.minor() == ver.minor()
+                    && this.patch() == ver.patch()
+                    && Arrays.equals(this.prerelease(), ver.prerelease());
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(@NotNull SemanticVersion o) {
+        int major = Integer.compare(this.major(), o.major());
+        if (major != 0) return major;
+
+        int minor = Integer.compare(this.minor(), o.minor());
+        if (minor != 0) return minor;
+
+        int patch = Integer.compare(this.patch(), o.patch());
+        if (patch != 0) return patch;
+
+        int highest = Math.min(o.prerelease().length, this.prerelease().length);
+
+        if (highest == 0) {
+            return Integer.compare(o.prerelease().length, this.prerelease().length);
+        }
+
+        for (int i = 0; i < highest; i++) {
+            int prerelease = o.prerelease()[i].compareTo(this.prerelease()[i]);
+            if (prerelease != 0) return prerelease;
+        }
+
+        return highest;
+    }
+}
---
+++ squirtgun-api/src/test/java/net/lucypoulton/squirtgun/tests/VersionTests.java
@@ -0,0 +1,41 @@
+package net.lucypoulton.squirtgun.tests;
+
+import net.lucypoulton.squirtgun.util.SemanticVersion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+
+public class VersionTests {
+    @Test
+    public void testParseBasic() {
+        Assertions.assertEquals(SemanticVersion.parse("1.2.3"), new SemanticVersion(1, 2, 3));
+    }
+
+    @Test
+    public void testParseWithPrereleaseAndMeta() {
+        Assertions.assertEquals(SemanticVersion.parse("1.2.3-pre+meta"),
+                new SemanticVersion(1, 2, 3, "meta", new String[]{"pre"}));
+    }
+
+    @Test
+    public void testFailWithInvalidCharacters() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> SemanticVersion.parse("a.b.c"));
+    }
+
+    @Test
+    public void testFailWithMissingVersions() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> SemanticVersion.parse("1.2"));
+    }
+
+    @Test
+    public void testOrderBasic() {
+        Assertions.assertTrue(SemanticVersion.parse("1.2.3").compareTo(SemanticVersion.parse("1.2.4")) < 0);
+    }
+
+    @Test
+    public void testPrereleaseOrder() {
+        Assertions.assertTrue(SemanticVersion.parse("1.0.0").compareTo(SemanticVersion.parse("1.0.0-pre")) > 0);
+        Assertions.assertTrue(SemanticVersion.parse("1.0.0-a").compareTo(SemanticVersion.parse("1.0.0-b")) > 0);
+        Assertions.assertTrue(SemanticVersion.parse("1.0.0-a.a").compareTo(SemanticVersion.parse("1.0.0-a")) > 0);
+    }
+}
